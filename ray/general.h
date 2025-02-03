0b3989abf822291c1431c26eb0793693ba4040b3
阿清 <qing@wireless-10-147-85-189.public.utexas.edu>
2025-02-03 14:36:15 -0600

    Add ray directory and ignore .DS_Store

---
+++ ray/general.h
@@ -0,0 +1,22 @@
+#ifndef __GENERAL_H__
+#define __GENERAL_H__
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <iostream>
+#include <string>
+
+class Exception {
+public:
+  Exception(const string &m) : msg(m) {}
+
+  string getMsg() const { return msg; }
+
+private:
+  string msg;
+};
+
+inline ostream &operator<<(ostream &os, const Exception &x) {
+  return os << x.getMsg();
+}
+
+#endif // __GENERAL_H__
